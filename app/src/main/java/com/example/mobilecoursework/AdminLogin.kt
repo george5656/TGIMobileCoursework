74567129410037971074c6fe7d62eda589b0bcee
George Finch <P2654772@my365.dmu.ac.uk>
2023-12-06 18:14:31 +0000

    started messing about with the database

--- app/src/main/java/com/example/mobilecoursework/AdminLogin.kt
+++ app/src/main/java/com/example/mobilecoursework/AdminLogin.kt
@@ -1,22 +1,48 @@
 package com.example.mobilecoursework
 
 import android.content.Intent
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.view.isVisible
+import com.example.mobilecoursework.model.DatabaseHelper
 
 class AdminLogin : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_login)
     }
 
     fun login(view: View){
-        var loginIntent : Intent = Intent(this, AdminHomePage::class.java)
-startActivity(loginIntent)
+
+        var DBHelper : DatabaseHelper = DatabaseHelper(this)
+
+        var Db: SQLiteDatabase = DBHelper.readableDatabase
+
+        var query: String = "select * from OrderDetails"
+        var output: Cursor? = Db.rawQuery(query,null)
+       // output.moveToFirst()
+//
+       // if(output.getString(5) == findViewById<EditText>(R.id.etPassword).text.toString()) {
+//
+//
+       //    var loginIntent: Intent = Intent(this, AdminHomePage::class.java)
+       //     startActivity(loginIntent)
+       //}
+var errorMessage :TextView = findViewById(R.id.txtErrorMessage)
+        errorMessage.setText("error")
+        errorMessage.isVisible = true
     }
     fun cancel(view: View){
         var cancelIntent : Intent = Intent(this, MainActivity::class.java)
+        startActivity((cancelIntent))
     }
 
 
--- app/src/main/java/com/example/mobilecoursework/model/DatabaseHelper.kt
+++ app/src/main/java/com/example/mobilecoursework/model/DatabaseHelper.kt
@@ -5,13 +5,89 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class DatabaseHelper(context:Context) :SQLiteOpenHelper(context,"cafeDatabase.db",null,1){
-    override fun onCreate(p0: SQLiteDatabase?) {
-        TODO("Not yet implemented")
+    override fun onCreate(db: SQLiteDatabase?) {
+
+        var   SQlCreateStament :String = ""
+                SQlCreateStament = "CREATE TABLE Customers ("+
+                                "cusId INTEGER NOT NULL UNIQUE,"+
+                                "cusFullName TEXT NOT NULL,"+
+                                "cusEmail TEXT NOT NULL,"+
+                                "cusPhoneNo TEXT NOT NULL,"+
+                                "cusUserName TEXT NOT NULL UNIQUE,"+
+                                "cusPassword TEXT NOT NULL,"+
+                                "CusIsActive INTEGER NOT NULL,"+
+                                "PRIMARY KEY(cusId AUTOINCREMENT));"
+
+
+
+
+
+              SQlCreateStament = SQlCreateStament + "CREATE TABLE Admin (" +
+                                        "adminId INTEGER NOT NULL UNIQUE,"+
+                                        "adminFullName TEXT NOT NULL,"+
+                                        "AdminEmail TEXT NOT NULL,"+
+                                        "adminPhoneNo TEXT NOT NULL,"+
+                                        "adminUserName TEXT NOT NULL UNIQUE,"+
+                                        "adminPassword TEXT NOT NULL,"+
+                                        "AdminIsActive INTEGER NOT NULL,"+
+                                        "PRIMARY KEY(adminId AUTOINCREMENT));"
+
+
+       SQlCreateStament = SQlCreateStament + "CREATE TABLE Product ("+
+                            "productId INTEGER NOT NULL UNIQUE,"+
+                            "prodName TEXT NOT NULL,"+
+                            "prodPrice REAL NOT NULL,"+
+                            "prodImage BLOB,"+
+                            "prodAvailable	INTEGER NOT NULL,"+
+                            "PRIMARY KEY(productId AUTOINCREMENT));"
+
+
+        SQlCreateStament = "CREATE TABLE Order ("+
+                            "orderId INTEGER NOT NULL UNIQUE,"+
+                            "cusId INTEGER NOT NULL,"+
+                            "orderData TEXT NOT NULL,"+
+                            "orderTime TEXT NOT NULL,"+
+                            "orderStatus TEXT NOT NULL,"+
+                            "PRIMARY KEY(orderId AUTOINCREMENT),"+
+                            "FOREIGN KEY(cusId) REFERENCES Customers(cusId));"
+
+
+       SQlCreateStament =  SQlCreateStament + "CREATE TABLE Payment ("+
+                            "paymentId INTEGER NOT NULL UNIQUE,"+
+                            "orderId INTEGER NOT NULL,"+
+                            "paymentType TEXT NOT NULL,"+
+                            "Amount	INTEGER NOT NULL,"+
+                            "paymentDate TEXT NOT NULL,"+
+                            "FOREIGN KEY(orderId) REFERENCES Order(orderId),"+
+                                "PRIMARY KEY(paymentId AUTOINCREMENT));"
+
+
+        SQlCreateStament =  SQlCreateStament + "CREATE TABLE Feedback ("+
+                                "feedbackId	INTEGER NOT NULL UNIQUE,"+
+                                "cusId INTEGER NOT NULL,"+
+                                "orderId INTEGER NOT NULL,"+
+                                "feedback TEXT NOT NULL,"+
+                                "rating INTEGER NOT NULL,"+
+                                "PRIMARY KEY(feedbackId AUTOINCREMENT),"+
+                                "FOREIGN KEY(cusId) REFERENCES Customers(cusId),"+
+                                "FOREIGN KEY(orderId) REFERENCES Order(orderId));"
+
+
+        SQlCreateStament =  SQlCreateStament + "CREATE TABLE OrderDetails ("+
+                                "orderDetailsID	INTEGER NOT NULL UNIQUE,"+
+                                "orderId INTEGER NOT NULL,"+
+                                "prodId	INTEGER NOT NULL,"+
+                                "PRIMARY KEY(orderDetailsID AUTOINCREMENT),"+
+                                "FOREIGN KEY(orderId) REFERENCES Order(orderId),"+
+                                "FOREIGN KEY(prodId) REFERENCES Product(productId));"
+        db?.execSQL(SQlCreateStament)
+
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
         TODO("Not yet implemented")
     }
 
-
 }
+
+
