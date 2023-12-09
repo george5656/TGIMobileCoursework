ac87c4bebaae7f0b6b01ea6a41a5155a08815f37
George Finch <P2654772@my365.dmu.ac.uk>
2023-12-09 11:12:45 +0000

    just made some more UI, and started making the toolbars to see how they worked

--- app/src/main/java/com/example/mobilecoursework/AdminSendPromotions.kt
+++ app/src/main/java/com/example/mobilecoursework/AdminSendPromotions.kt
@@ -7,5 +7,6 @@ class AdminSendPromotions : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_send_promotions)
+
     }
 }
