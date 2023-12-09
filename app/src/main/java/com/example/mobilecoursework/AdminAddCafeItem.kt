5ca4b157200e78a6814de3dc7719b219d540acef
George Finch <P2654772@my365.dmu.ac.uk>
2023-12-09 12:51:53 +0000

    more progress on the UI and i also, got the file chooser to open up.

--- app/src/main/java/com/example/mobilecoursework/AdminAddCafeItem.kt
+++ app/src/main/java/com/example/mobilecoursework/AdminAddCafeItem.kt
@@ -1,11 +1,20 @@
 package com.example.mobilecoursework
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class AdminAddCafeItem : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_add_cafe_item)
     }
+    fun imageLoader(view : View){
+        var fileChoserIntent : Intent = Intent()
+        fileChoserIntent.setType("image/*")
+        fileChoserIntent.setAction(Intent.ACTION_GET_CONTENT)
+         startActivity(fileChoserIntent)
+    }
+
 }
